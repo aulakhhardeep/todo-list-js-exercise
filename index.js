6e32f7307ca0ecfda1c8a7fbcdf55bc671f6531a
Hardeep Kaur Aulakh <aulakh.hardeep91@gmail.com>
2023-04-15 18:30:56 -0600

    Added refractor functions

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-// Arrays to keep track of each task's state
+function newTask(title, description) {
+  const task = {
+    title: title,
+    description: description,
+    complete: false
+  };
+  return task;
+}
+
+//prints out the provided task's details
+function logTaskState(task) {
+  console.log(`${task.title} has ${task.complete ? " ": " not"} been completed`);
+}
+
+//marks the provided task as completed
+function completeTask(task) {
+  task.complete = true;
+}
+
+//Driver code BELOW
+const task1 = newTask ("Clean Cat Litter", "Take all the 💩 out of the litter box");
+const task2 = newTask("Do Laundry","😨");
+const tasks = [task1, task2];
+
+logTaskState(task1); //Clean Cat litter has not been completed
+completeTask(task1);
+logTaskState(task1); //Clean Cat Litter has been completed.
+// for now, let's just make sure we see our tasks
+console.log(tasks);
+
+/** // Arrays to keep track of each task's state
 const taskTitles = [];
 const taskComplete = [];
+const taskDescriptions = [];
 
 // Create a new task by adding to the arrays
 // A new task will be created as incomplete
-function newTask(title) {
+function newTask(title, description) {
   taskTitles.push(title);
+  taskDescriptions.push(description);
   taskComplete.push(false);
 }
 
@@ -29,3 +61,4 @@ newTask("Do Laundry"); // task 1
 logTaskState(0); // Clean Cat Litter has not been completed
 completeTask(0);
 logTaskState(0); // Clean Cat Litter has been completed
+*/
